bd130f70519eede9415d3765228cdeefe8e73476
weijun huang <luck938938@hotmail.com>
2018-06-28 02:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function updateObjectWithKeyAndValue (object, key, value)
 {
   return Object.assign({}, object, {[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndVakye(object, key, value)
+{
+  object[key]= value
+  return object
+}
