ed0ca580541c5f4c2b935081e26b7426e1d6bb28
weijun huang <luck938938@hotmail.com>
2018-06-28 02:55:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 
 function deleteFromObjectByKey(object, key)
 {
-  delete object[key]
+  delete object.key
   return object
 }
