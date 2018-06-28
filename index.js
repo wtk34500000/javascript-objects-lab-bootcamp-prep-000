0292213caf522b08ad27d19836e35d2bbb1f02bb
weijun huang <luck938938@hotmail.com>
2018-06-28 03:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function destructDeleteFromObjectByKey(object, key)
 function destructivelyDeleteFromObjectByKey(object, key)
 {
   delete object.key
-  return obj
+  return objdestructivelyDeleteFromObjectByKey
 }
 
 function destructivelyDeleteFromObjectByKey(object, key)
